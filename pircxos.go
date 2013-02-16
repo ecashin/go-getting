f9790141384a32dbeab3795dbee0231658f4fe83
Ed Cashin <ecashin@noserose.net>
2013-02-15 21:08:35 -0500

    document messages; split with regexps

--- pircxos.go
+++ pircxos.go
@@ -1,7 +1,28 @@
 // test IRC client uses https://github.com/husio/go-irc.git
 // based on examples/client.go in go-irc
 //
-// GOPATH="$HOME"/git/go-irc go build ircctest.go
+// GOPATH="$HOME"/git/go-irc go run pircxos.go
+//
+// Message formats:
+//
+// G P propose		leader (AKA proposer) proposes number P for 
+// 			game G
+//
+// G P promise Q V	acceptor promises not to accept proposal
+// 			with number less than P in game G, telling
+//			proposer that value V has already been accepted
+//			for proposal number Q in game G.  A nil V means
+//			no value has been accepted yet.
+// 
+// G P set V		leader asks acceptors to accept value
+//			V for proposal number P in game G.  It should
+//			be the V with the highest Q with a non-nil V,
+//			or a V of the leaders chosing if no promise had
+//			a non-nil V.
+//
+// G P accept V		acceptor accepts value V.  This acceptor will
+//			accept a different value with a higher proposal
+//			number, though, if one comes.
 
 package main
 
@@ -12,6 +33,7 @@ import (
 	"irc"
 	"log"
 	"os"
+	"regexp"
 	"strings"
 )
 
@@ -63,8 +85,20 @@ func (pm *PMod) playerstr() string {
 	return fmt.Sprintf("%d", len(pm.players))
 }
 
+//  :bobobobono!~ecashin@hosty.example.com JOIN #pmodtesting
+//  :bobobobono!~ecashin@hosty.example.com QUIT :Client Quit
+//  :wowowowowon!~ecashin@hosty.example.com JOIN #pmodtesting
+//  :wowowowowon!~ecashin@hosty.example.com PART #pmodtesting
+//  PING :verne.freenode.net
+//  :wowowowowon!~ecashin@hosty.example.com JOIN #pmodtesting
+//  :wowowowowon!~ecashin@hosty.example.com PRIVMSG #pmodtesting :hello there
 func (pm *PMod) handle(send func(string), m string) bool {
 	cont := true
+	re := regexp.MustCompile(":(\\w+?)!~(\\S+?)@(\\S+?)\\s+(\\S+)\\s+(.*)")
+	g := re.FindStringSubmatch(m)
+	for i, v := range g {
+		log.Println(i, v)
+	}
 	search := " PRIVMSG #" + pm.ircchan + " :"
 	i := strings.Index(m, search)
 	log.Printf("i:%d", i)
