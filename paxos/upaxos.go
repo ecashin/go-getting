6b610ef0fea8b4c73e9622e12fbf15cb2b554b0e
Ed Cashin <ecashin@noserose.net>
2013-04-19 22:01:34 -0400

    Add skeleton of UDP Paxos participant

---
+++ paxos/upaxos.go
@@ -0,0 +1,104 @@
+// upaxos.go - UDP-based Paxos participant implementation
+//
+//	The peers are expected to be supplied line-by-line
+//	on standard input, in the form: a.b.c.d:p, an IP
+//	address a.b.c.d and port p.
+
+package main
+
+import (
+	"bufio"
+	"container/list"
+	"flag"
+	"fmt"
+	"log"
+	"net"
+	"os"
+	"strings"
+)
+
+var myAddr string
+var myID int = -1
+
+func group() []string {
+	grp := list.New()
+	in := bufio.NewReader(os.Stdin)
+	line, err := in.ReadSlice('\n')
+	i := 0
+	for err == nil {
+		fields := strings.Fields(string(line))
+		if len(fields) > 0 {
+			s := fields[0]
+			grp.PushBack(s)
+			if fields[0] == myAddr {
+				myID = i
+				s = fmt.Sprintf("  me: %s %d", s, i)
+			} else {
+				s = "peer: " + s
+			}
+			i += 1
+			log.Print(s)
+		}
+		line, err = in.ReadSlice('\n')
+	}
+	g := make([]string, i)
+	i = 0
+	for e := grp.Front(); e != nil; e = e.Next() {
+		g[i] = e.Value.(string)
+		i++
+	}
+	if myID == -1 {
+		log.Panic("could not determine my proposal number set")
+	}
+	return g
+}
+
+func serve(c chan string) {
+	la, err := net.ResolveUDPAddr("udp4", myAddr)
+	if err != nil {
+		log.Panic(err)
+	}
+	conn, err := net.ListenUDP("udp4", la)
+	if err != nil {
+		log.Panic(err)
+	}
+	buf := make([]byte, 9999)
+	for {
+		n, raddr, err := conn.ReadFromUDP(buf)
+		if err != nil {
+			log.Panic(err)
+		}
+		s := string(buf[:n])
+		if len(strings.Fields(s)) == 0 {
+			continue
+		}
+		c <- s
+		rsp := <-c
+		_, err = conn.WriteToUDP([]byte(rsp), raddr)
+		if err != nil {
+			log.Panic(err)
+		}
+	}
+}
+
+func init() {
+	flag.StringVar(&myAddr, "a", "127.0.0.1:9876",
+		"IP and port for this process")
+}
+func main() {
+	flag.Parse()
+	log.Print("upaxos started at ", myAddr)
+	defer log.Print("upaxos ending")
+
+	g := group()
+	for i := 0; i < len(g); i++ {
+		log.Print(g[i])
+	}
+	sc := make(chan string)
+	go serve(sc)
+	for {
+		m := <- sc
+		log.Print(m)
+		sc <- fmt.Sprintf(">%s<\n", strings.TrimSpace(m))
+	}
+}
