814246cda8de668974eabc5331958d655a68dd45
Ed Cashin <ed.cashin@acm.org>
2014-04-17 22:37:17 -0400

    Support multiple view models in WebSocket messages

--- web/shareform/shform.js
+++ web/shareform/shform.js
@@ -4,10 +4,18 @@ var shform = shform || {};
 $(document).ready(function () {
     "use strict";
 
-    var wsconn,                     // WebSocket connection
-    last_received,
-    send = function (msg) {
-        if (msg === last_received) {
+    var wsconn,                 // WebSocket connection
+    prop,                       // property
+    lastReceived,
+    send = function (vm, prop, val) {
+        var viewModel = shform.viewModels[vm],
+        msg = JSON.stringify({
+            "vm" : vm,
+            "prop" : prop,
+            "val" : val
+        });
+
+        if (msg === lastReceived) {
             console.log("not sending last received");
             return;
         }
@@ -17,29 +25,37 @@ $(document).ready(function () {
         } else {
             console.log("no ws.  NOT sending message (" + msg + ")");
         }
+    },
+    setupWs = function () {
+        var msg, viewModel;
+
+        if (window["WebSocket"]) {
+            console.log("setting up WebSocket");
+            wsconn = new WebSocket("{{$}}");
+            wsconn.onclose = function(evt) {
+                console.log("connection closed");
+            }
+            wsconn.onmessage = function(evt) {
+                console.log("received: " + evt.data);
+                msg = $.parseJSON(evt.data);
+                lastReceived = JSON.stringify(msg);
+                viewModel = shform.viewModels[msg.vm];
+                viewModel[msg.prop](msg.val);
+            }
+            shform.wsconn = wsconn;
+        } else {
+            // XXXtodo: Add fall-back (e.g., to long polling) here.
+            $("p.lead").html("Sorry.  Your browser does not support WebSockets.");
+            console.log("no ws support in browser");
+        }
     };
 
-    if (window["WebSocket"]) {
-        console.log("setting up WebSocket");
-        wsconn = new WebSocket("{{$}}");
-        wsconn.onclose = function(evt) {
-            console.log("connection closed");
-        }
-        wsconn.onmessage = function(evt) {
-            console.log("received: " + evt.data);
-            last_received = evt.data;
-            shform.viewModel.bandVal(evt.data);
-        }
-        shform.wsconn = wsconn;
-    } else {
-        // XXXtodo: Add fall-back (e.g., to long polling) here.
-        $("p.lead").html("Sorry.  Your browser does not support WebSockets.");
-        console.log("no ws support in browser");
-    }
+    setupWs();
 
     // based on example in Knockout docs:
     // http://knockoutjs.com/documentation/rateLimit-observable.html
-    function AppViewModel() {
+    function BandViewModel() {
+        this.name = "band";
         this.bandVal = ko.observable();
         this.bandSlowVal = ko.computed(this.bandVal)
             .extend({
@@ -53,10 +69,16 @@ $(document).ready(function () {
         this.loggedValues = ko.observableArray([]);
         this.bandSlowVal.subscribe(function (val) {
             this.loggedValues.push(val);
-            send(val);
+            send(this.name, "bandVal", val);
         }, this);
     }
 
-    shform.viewModel = new AppViewModel();
-    ko.applyBindings(shform.viewModel, $('#bandDiv').get(0));
+    shform.viewModels = {};
+    shform.viewModels.band = new BandViewModel();
+    for (prop in shform.viewModels) {
+        if (!shform.viewModels.hasOwnProperty(prop)) {
+            continue;
+        }
+        ko.applyBindings(shform.viewModels[prop], $('#' + prop + 'Div').get(0));
+    }
 });
