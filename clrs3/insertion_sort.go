3158690fa44277aa70dde1214b9a27d8a01e8def
Ed Cashin <ed.cashin@acm.org>
2014-04-26 10:45:20 -0400

    Stub in the input mechanics

--- clrs3/insertion_sort.go
+++ clrs3/insertion_sort.go
@@ -1,6 +1,22 @@
 package main
 
-import ()
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
 
 func main() {
+	in := bufio.NewReader(os.Stdin)
+	line, err := in.ReadBytes('\n')
+	a := []string{}
+	for err == nil {
+		flds := strings.Fields(string(line))
+		for _, i := range flds {
+			a = append(a, i)
+		}
+		line, err = in.ReadBytes('\n')
+	}
+	fmt.Printf("%q", a)
 }
