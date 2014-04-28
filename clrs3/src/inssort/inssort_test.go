11b56b4771aae3ffda9ba0808bc529599aa86c20
Ed Cashin <ed.cashin@acm.org>
2014-04-27 20:35:58 -0400

    Actually verify stuff in the test

--- clrs3/src/inssort/inssort_test.go
+++ clrs3/src/inssort/inssort_test.go
@@ -1,10 +1,16 @@
 package inssort
 
 import (
-	"fmt"
 	"testing"
 )
 
 func TestTrial(t *testing.T) {
-	fmt.Println("Test function runs.")
+	a := []int64{2, 3, 4, 2, 1}
+	ref := []int64{1, 2, 2, 3, 4}
+	Sort(a)
+	for i := 0; i < len(a); i++ {
+		if a[i] != ref[i] {
+			t.Errorf("a[%d] == %d", i, a[i])
+		}
+	}
 }
