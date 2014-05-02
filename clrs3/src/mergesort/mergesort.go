cac1a7a0c0701d80747cc974044a1a5bd561a6bc
Ed Cashin <ed.cashin@acm.org>
2014-05-01 22:12:31 -0400

    Use Go-ized merge in sort routine

--- clrs3/src/mergesort/mergesort.go
+++ clrs3/src/mergesort/mergesort.go
@@ -1,31 +1,28 @@
 // CLRS3 2.3.1
 package mergesort
 
-import (
-	"math"
-)
-
-const (
-	Infinity = math.MaxInt64
-)
-
+/* CLRS3 has 1-based indexing, but Go doesn't.
+ * Here we're merging a[p:q] and a[q:r] with more idiomatic Go.
+ */
 func merge(a []int64, p int, q int, r int) {
-	n1 := q - p + 1
-	n2 := r - q + 1
-	left := make([]int64, n1)
-	right := make([]int64, n2)
-	for i := 0; i < n1-1; i++ {
+	left := make([]int64, q-p)
+	right := make([]int64, r-q)
+	for i := 0; i < len(left); i++ {
 		left[i] = a[p+i]
 	}
-	for j := 0; j < n2-1; j++ {
+	for j := 0; j < len(right); j++ {
 		right[j] = a[q+j]
 	}
-	left[n1-1] = Infinity
-	right[n2-1] = Infinity
 	i := 0
 	j := 0
 	for k := p; k < r; k++ {
-		if left[i] < right[j] {
+		if i >= len(left) {
+			a[k] = right[j]
+			j += 1
+		} else if j >= len(right) {
+			a[k] = left[i]
+			i += 1
+		} else if left[i] < right[j] {
 			a[k] = left[i]
 			i += 1
 		} else {
@@ -35,5 +32,15 @@ func merge(a []int64, p int, q int, r int) {
 	}
 }
 
+func sort(a []int64, p int, r int) {
+	if r-p > 1 {
+		q := (p + r) / 2
+		sort(a, p, q)
+		sort(a, q, r)
+		merge(a, p, q, r)
+	}
+}
+
 func Sort(a []int64) {
+	sort(a, 0, len(a))
 }
--- clrs3/src/mergesort/mergesort_test.go
+++ clrs3/src/mergesort/mergesort_test.go
@@ -6,19 +6,24 @@ import (
 
 func TestMerge(t *testing.T) {
 	a := []int64{1, 3, 5, 2, 4, 6}
+	ref := []int64{1, 2, 3, 4, 5, 6}
 	merge(a, 0, 3, 6)
-	t.Log(a)
+	for i, v := range a {
+		if v != ref[i] {
+			t.Errorf("(a[%d]==%d) != (ref[%d]==%d)",
+				i, a[i], i, ref[i])
+		}
+	}
 }
 
-/*
 func TestSorting(t *testing.T) {
 	a := []int64{2, 3, 4, 2, 1}
 	ref := []int64{1, 2, 2, 3, 4}
 	Sort(a)
-	for i := 0; i < len(a); i++ {
-		if a[i] != ref[i] {
-			t.Errorf("a[%d] == %d", i, a[i])
+	for i, v := range a {
+		if v != ref[i] {
+			t.Errorf("(a[%d]==%d) != (ref[%d]==%d)",
+				i, v, i, ref[i])
 		}
 	}
 }
-*/
