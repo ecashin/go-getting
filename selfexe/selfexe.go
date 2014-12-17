686d6eb43a130fc99540c5db3440e7dcd1fbcb7f
Ed Cashin <ed.cashin@acm.org>
2014-12-16 20:58:31 -0500

    Add execution of payload

--- selfexe/selfexe.go
+++ selfexe/selfexe.go
@@ -3,14 +3,26 @@ package main
 import (
 	"fmt"
 	"os"
+	"os/exec"
 )
+
+const exe = "/tmp/selfexe-payload"
 
 func main() {
 	fmt.Println("hi", len(Bin))
-	f, err := os.OpenFile("/tmp/selfexe-payload", os.O_WRONLY|os.O_CREATE, 0755)
+	f, err := os.OpenFile(exe, os.O_WRONLY|os.O_CREATE, 0755)
 	if err != nil {
 		panic(err)
 	}
 	f.WriteString(Bin)
 	f.Close()
+
+	cmd := exec.Command(exe)
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	err = cmd.Run()
+	if err != nil {
+		panic(err)
+	}
+	cmd.Wait()
 }
