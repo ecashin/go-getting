2f22bc557f868c4fd31d806989ef88ae9301af4a
Ed Cashin <ed.cashin@acm.org>
2016-04-17 22:03:59 -0400

    Show only most common value(s)

--- wc.go
+++ wc.go
@@ -6,17 +6,21 @@
 // the pipe system call to connect that standard output to the
 // standard input of the second command.
 // 
-// The second command tells go to compile the Go source file, wc.go,
-// and then to run the compiled binary.
+// The second command, "go run wc.go", tells go to compile the Go
+// source file, wc.go, and then to run the compiled binary.
 //
-// The compiled binary prints three lines, showing the counts for each
-// of the three "words": "a", "b", and "c".
+// The compiled binary prints the list of most-common words inside
+// square brackets.
+//
+// In the first run, there's one mode, and in the second run, there
+// are two.
 // 
 // ecashin@montgomery:~/git/go-getting$ echo a b b c c c | go run wc.go
-// a 1
-// b 2
-// c 3
+// [c]
+// ecashin@montgomery:~/git/go-getting$ echo d d d a b b c c c | go run wc.go
+// [d c]
 // ecashin@montgomery:~/git/go-getting$ 
+
 
 // This Go source file implements resources inside the "main" package,
 // so that it can be the core of a running program.
@@ -68,6 +72,13 @@ func main() {
 		counts[scanner.Text()]++
 	}
 
+	// Declare an integer for storing the maximum count.
+	max := 0
+
+	// Declare a slice of string with zero elements.  (Just think
+	// of it as an array for now.)  It will store the modes.
+	modes := make([]string, 0)
+
 	// This loop goes over every key/value pair in the map.
 	//
 	// The keys will be words, like "a", "b", and "c" in the
@@ -76,7 +87,22 @@ func main() {
 	//
 	// The .Println function in the fmt package is like Java's
 	// System.out.Println.
-	for key, value := range counts {
-		fmt.Println(key, value)
+	for word, count := range counts {
+		// If the current counter is larger than the maximum
+		// count seen so far, then reset the list of modes
+		// with a slice containing only this word.  Record the
+		// new maximum.
+		if count > max {
+			modes = []string{word}
+			max = count
+		} else if count == max {
+			// Append any other word with the same maximal
+			// count to the mode list.
+			modes = append(modes, word)
+		}
 	}
+
+	// Print a line to standard output using the default
+	// stringification for the value.
+	fmt.Println(modes)
 }
